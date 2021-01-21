d6b48d64ec752d901127164306afdce51682804d
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-21 15:53:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,8 @@ def using_push (array, string)
   new_array = ["strings", "in", "this", "array"]
   new_array.push "violet"
 end
+
+def using_unshift(array, string)
+  new_array = ["strings", "in", "this", "array"]
+  new_array.unshift "violet"
+end
